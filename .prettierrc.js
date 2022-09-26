9422a352c1916622114141d3df9e4481528931b4
hslee <gkrtn632@gmail.com>
2022-09-27 01:42:29 +0900

    eslint setting

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  endOfLine: "auto",
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  jsxBracketSameLine: true,
+  trailingComma: "none",
+  tabWidth: 2,
+  usePrettierrc: false,
+  printWidth: 120,
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  parser: "typescript",
+};
